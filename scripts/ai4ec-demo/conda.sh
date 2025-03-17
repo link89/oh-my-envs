78ebdedcd01950414dd329181dc21a013a5dc100
weihong.xu <xuweihong.cn@gmail.com>
2025-03-17 16:18:13 +0800

    update cuda

--- scripts/ai4ec-demo/conda.sh
+++ scripts/ai4ec-demo/conda.sh
@@ -10,8 +10,10 @@ set -e
 [ -f "Miniconda3-latest-Linux-x86_64.sh" ] ||
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
+
 ./Miniconda3-latest-Linux-x86_64.sh -b -p ~/miniconda3
 ~/miniconda3/bin/conda init bash
+
 SSH_EOF
 touch conda.done
 }
